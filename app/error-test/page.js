bdf362d415c39acbe57edfec8d5713f9a9c8d263
GodhaniyaArbham <arbham.stackdot@outlook.com>
2026-07-06 15:01:27 +0530

    feat: add contact page with API integration, error testing route, and custom 404 page

--- app/error-test/page.js
+++ app/error-test/page.js
@@ -1,3 +1,13 @@
 export default function ErrorTestPage() {
   throw new Error("This is a normal error for testing");
 }
+
+
+/*export default function ErrorTestPage() {
+  return (
+    <div>
+      <h1>Error Test Page</h1>
+      <p>No error now.</p>
+    </div>
+  );
+}  */
